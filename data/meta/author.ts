d6393d792e2a4286dfa810b9c8e68958d12c99b5
JasonLamv-t <jasonlamv-t@hotmail.com>
2023-01-09 20:37:03 +0800

    update: add data folder.

---
+++ data/meta/author.ts
@@ -0,0 +1,20 @@
+const authorData = [
+  {
+    name: 'Jason Lam',
+    avatar: '/static/images/avatar-22.jpg',
+    occupation: 'Full Stack Engineer',
+    company: '',
+    organize: '',
+    location: 'Shenzhen, China',
+    social: {
+      email: 'jasonlamv-t@hotmail.com',
+      github: 'https://github.com/JasonLamv-t',
+      // twitter: 'https://twitter.com/Twitter',
+      // facebook: 'https://facebook.com',
+      // youtube: 'https://youtube.com',
+      linkedin: 'https://www.linkedin.cn/injobs/in/jason-lam-0827181b0'
+    }
+  }
+];
+
+export default authorData;
---
+++ data/meta/site.ts
@@ -0,0 +1,17 @@
+const siteData = {
+  title: 'Brachistochrone',
+  headerTitle: 'Brachistochrone',
+  author: 'Jason Lam',
+  description: "Jason Lam's personal website",
+  language: 'zh-cn, en-us',
+  theme: 'system', // system, dark or light
+  siteUrl: 'https://jasonlam.cc',
+  // siteRepo: 'https://github.com/JasonLamv-t/jasonlamv-t.github.io',
+  // siteLogo: '/static/images/logo.jpg',
+  // beian: '粤ICP备2021039182号-1',
+  // image: '/static/images/avatar-22.jpg',
+  // socialBanner: '/static/images/twitter-card.png',
+  locale: 'zh-CN'
+};
+
+export default siteData;
