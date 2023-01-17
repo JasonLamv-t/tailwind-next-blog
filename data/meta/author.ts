238ac3cf30b736fa90d0255e86bdd8f87eb9f0ec
JasonLamv-t <jasonlamv-t@hotmail.com>
2023-01-17 16:28:44 +0800

    update: site and author meta and add interface.

--- data/meta/author.ts
+++ data/meta/author.ts
@@ -1,14 +1,34 @@
-const authorData = {
+interface AuthorData {
+  name: string;
+  avatar: string;
+  occupation: string;
+  company: string;
+  organization: string;
+  location: string;
+  social: {
+    email?: string;
+    github?: string;
+    linkedin?: string;
+    twitter?: string;
+    facebook?: string;
+    youtube?: string;
+    acfun?: string;
+    bilibili?: string;
+  };
+}
+
+const authorData: AuthorData = {
   name: 'Jason Lam',
   avatar: '/static/images/avatar-22.jpg',
   occupation: 'Full Stack Engineer',
   company: '',
-  organize: '',
+  organization: '',
   location: 'Shenzhen, China',
   social: {
     email: 'jasonlamv-t@hotmail.com',
     github: 'JasonLamv-t',
-    linkedin: 'https://www.linkedin.cn/injobs/in/jason-lam-0827181b0'
+    linkedin: 'https://www.linkedin.cn/injobs/in/jason-lam-0827181b0',
+    bilibili: '10905363'
     // twitter: 'JasonLamv-t',
     // facebook: 'jasonlamv.t',
     // youtube: '@jasonlamv-t'
--- data/meta/site.ts
+++ data/meta/site.ts
@@ -1,20 +1,32 @@
-const siteData = {
+interface SiteData {
+  title: string;
+  headerTitle: string;
+  author: string;
+  description: string;
+  locale: string;
+  language: string;
+  theme: 'system' | 'light' | 'dark';
+  url: string;
+  logo: string;
+  banner: string;
+  beian?: string;
+  siteRepo?: string;
+}
+
+const siteData: SiteData = {
   title: 'Brachistochrone',
   headerTitle: 'Brachistochrone',
   author: 'Jason Lam',
   description: "Jason Lam's personal website",
   language: 'zh-cn, en-us',
   theme: 'system', // system, dark or light
-  url:
-    process.env.NODE_ENV === 'development'
-      ? 'http://localhost:3000'
-      : 'https://jasonlam.cc',
+  url: 'https://jasonlam.cc',
   logo: '/images/logo.jpg',
+  banner: '/images/pandas.jpg',
+  locale: 'zh-CN'
   // siteRepo: 'https://github.com/JasonLamv-t/jasonlamv-t.github.io',
   // beian: '粤ICP备2021039182号-1',
   // image: '/static/images/avatar-22.jpg',
-  banner: '/images/pandas.jpg',
-  locale: 'zh-CN'
 };
 
 export default siteData;
