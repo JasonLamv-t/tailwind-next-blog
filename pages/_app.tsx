e6ce5e1c93d798b99e0b9b113874d7201e19d4aa
JasonLamv-t <jasonlamv-t@hotmail.com>
2023-01-17 16:27:54 +0800

    update: add pages.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,12 @@
 import LayoutWrapper from 'components/LayoutWrapper';
+import siteData from 'data/meta/site';
 import { ThemeProvider } from 'next-themes';
 import type { AppProps } from 'next/app';
 import 'styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider attribute="class">
+    <ThemeProvider attribute="class" defaultTheme={siteData.theme}>
       <LayoutWrapper>
         <Component {...pageProps} />
       </LayoutWrapper>
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,3 +1,3 @@
-export default function Home() {
+export default function About() {
   return <h1 className="text-3xl font-bold underline">Hello about!</h1>;
 }
