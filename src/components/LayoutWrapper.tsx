12effc8015c2ba38bc87317957c9077eb692b0cc
JasonLamv-t <jasonlamv-t@hotmail.com>
2023-01-21 05:09:56 +0800

    update: add _document and set the background.

--- src/components/LayoutWrapper.tsx
+++ src/components/LayoutWrapper.tsx
@@ -5,7 +5,7 @@ import { ReactNode } from 'react';
 const LayoutWrapper = ({ children }: { children: ReactNode }) => {
   return (
     <>
-      <div className="container h-screen flex flex-col mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="container h-screen flex flex-col mx-auto px-4 sm:px-6 lg:px-8 bg-white ring-1 ring-zinc-100 dark:bg-zinc-900 dark:ring-zinc-300/20">
         <Header />
         <main className="mb-auto">{children}</main>
         <Footer />
---
+++ src/pages/_document.tsx
@@ -0,0 +1,13 @@
+import { Head, Html, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html>
+      <Head />
+      <body className="bg-zinc-50 dark:bg-black">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
