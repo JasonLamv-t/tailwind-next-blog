0dc1ef3291116da1db091bc4c53305905d23b915
JasonLamv-t <jasonlamv-t@hotmail.com>
2023-01-21 04:56:07 +0800

    deps: remove @fullhuman/postcss-purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,22 +1,3 @@
 module.exports = {
-  plugins: [
-    'tailwindcss',
-    'autoprefixer',
-    'postcss-preset-env',
-    process.env.NODE_ENV === 'production'
-      ? [
-          '@fullhuman/postcss-purgecss',
-          {
-            content: [
-              './src/pages/**/*.{js,jsx,ts,tsx}',
-              './src/components/**/*.{js,jsx,ts,tsx}',
-              './src/libs/**/*.{js,jsx,ts,tsx}'
-            ],
-            defaultExtractor: (content) =>
-              content.match(/[\w-/:]+(?<!:)/g) || [],
-            safelist: ['html', 'body']
-          }
-        ]
-      : null
-  ]
+  plugins: ['tailwindcss', 'autoprefixer', 'postcss-preset-env']
 };
