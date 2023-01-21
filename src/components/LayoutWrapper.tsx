7d0cdd60e1724a8e81d265d1d5ddc0ffa4e65167
JasonLamv-t <jasonlamv-t@hotmail.com>
2023-01-21 19:56:52 +0800

    update: adjust the layout.

--- src/components/LayoutWrapper.tsx
+++ src/components/LayoutWrapper.tsx
@@ -6,7 +6,7 @@ const LayoutWrapper = ({ children }: { children: ReactNode }) => {
   return (
     <>
       <div className="container mx-auto max-w-5xl px-4 sm:px-10 xl:px-16 bg-white ring-1 ring-zinc-100 dark:bg-zinc-900 dark:ring-zinc-300/20">
-        <div className="h-screen flex flex-col">
+        <div className="h-full min-h-screen flex flex-col">
           <Header />
           <main className="mb-auto">{children}</main>
           <Footer />
