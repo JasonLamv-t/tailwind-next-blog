a86da0e4741873f577995df70bff6de70a75eaad
JasonLamv-t <jasonlamv-t@hotmail.com>
2023-01-22 13:47:38 +0800

    update: libs

--- src/libs/blog.ts
+++ src/libs/blog.ts
@@ -1,8 +1,11 @@
+import siteData from '#/meta/site';
+import { BlogMeta } from '@/types/blog';
+
+import fg from 'fast-glob';
 import fs from 'fs';
 import matter from 'gray-matter';
 import { orderBy } from 'lodash-es';
 import path from 'path';
-import { getAllFilePaths } from './file';
 
 const blogsPath = 'data/blogs';
 
@@ -11,12 +14,28 @@ const blogsPath = 'data/blogs';
  * @returns all publish blogs' meta in date order
  */
 export const getAllBlogsMeta = () => {
-  const allFilePaths = getAllFilePaths(path.resolve(blogsPath));
-  const publishBlogsMatter = allFilePaths
-    .map((filepath) => {
-      const MDFile = fs.readFileSync(filepath, 'utf8');
-      return matter(MDFile).data;
+  const blogFilepaths = fg
+    .sync(['**/*.md', '**/*.mdx'], {
+      cwd: path.resolve(blogsPath),
+    })
+    .map((filename) => path.resolve(blogsPath, filename));
+
+  const publishedBlogsMeta = blogFilepaths
+    .map((filename): BlogMeta => {
+      const MDFile = fs.readFileSync(filename, 'utf8');
+      const meta: BlogMeta = matter(MDFile).data as BlogMeta;
+      meta.dateString = new Date(meta.date).toLocaleDateString(
+        require('#/meta/site').locale,
+        {
+          day: 'numeric',
+          month: 'long',
+          year: 'numeric',
+          timeZone: 'UTC',
+        }
+      );
+      return meta;
     })
     .filter(({ draft }) => !draft);
-  return orderBy(publishBlogsMatter, 'date', 'desc');
+
+  return orderBy(publishedBlogsMeta, 'date', 'desc');
 };
