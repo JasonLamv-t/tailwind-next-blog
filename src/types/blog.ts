e5cbdf2ffb4676da8f5c2ef24cf0d4df219e59a5
JasonLamv-t <jasonlamv-t@hotmail.com>
2023-01-22 13:48:19 +0800

    update: add types to decalre some interface of self-organie data.

---
+++ src/types/blog.ts
@@ -0,0 +1,10 @@
+export interface BlogMeta {
+  title: string;
+  date: string;
+  dateString?: string;
+  tags: string[];
+  draft: boolean;
+  summary: string;
+  image: string;
+  canonicalUrl: string;
+}
