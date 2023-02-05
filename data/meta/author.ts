c4020637eba9aa6072f7b0bb2f401348a047b5bd
JasonLamv-t <jasonlamv-t@hotmail.com>
2023-02-05 22:08:20 +0800

    seo: add blog seo component.

--- data/meta/author.ts
+++ data/meta/author.ts
@@ -28,11 +28,11 @@ const authorData: AuthorData = {
     email: 'jasonlamv-t@hotmail.com',
     github: 'JasonLamv-t',
     linkedin: 'https://www.linkedin.cn/injobs/in/jason-lam-0827181b0',
-    bilibili: '10905363'
-    // twitter: 'JasonLamv-t',
+    bilibili: '10905363',
+    twitter: 'JasonLamv-t',
     // facebook: 'jasonlamv.t',
     // youtube: '@jasonlamv-t'
-  }
+  },
 };
 
 export default authorData;
