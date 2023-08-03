4e679860a77f214a88caf0902523fda955c9005f
JasonLamv-t <jasonlamv-t@hotmail.com>
2023-08-04 01:23:15 +0800

    style: adjust the Warn

--- app/components/MDX/Note.tsx
+++ app/components/MDX/Note.tsx
@@ -15,7 +15,7 @@ const Info = ({ children }: { children: ReactNode }) => {
 
 const Warn = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="my-6 flex gap-2.5 rounded-2xl border border-red-300 bg-red-200/70 p-4 leading-6 text-red-800 dark:border-red-500/40 dark:bg-red-500/10 dark:text-red-200 dark:[--tw-prose-links:theme(colors.white)] dark:[--tw-prose-links-hover:theme(colors.red.300)]">
+    <div className="my-6 flex gap-2.5 rounded-2xl border border-red-300 bg-red-200/70 p-4 leading-6 text-red-800 dark:border-red-500/40 dark:bg-red-500/10 dark:text-yellow-200 dark:[--tw-prose-links:theme(colors.white)] dark:[--tw-prose-links-hover:theme(colors.yellow.300)]">
       <IconAlertTriangleFilled className="mt-0.5 h-5 w-5 flex-none fill-red-500 stroke-white dark:fill-yellow-500 dark:stroke-red-200" />
       <div className="[&>:first-child]:mt-0 [&>:last-child]:mb-0">
         {children}
